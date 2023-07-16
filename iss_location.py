b224f08520ea5230479d2e78802798401ad6d7aa
yamtimor <yamtimor@gmail.com>
2023-07-17 00:41:59 +0300

    Update changes

--- iss_location.py
+++ iss_location.py
@@ -7,6 +7,11 @@ iss_json = res.json()
 
 
 def main():
+    """
+    Connect to Wheretheiss API and plot the attractions on a map.
+    """
+
+    # The lat and lng of the attraction
     attractions_lats = iss_json['latitude']
     attractions_lngs = iss_json['longitude']
 
@@ -17,6 +22,7 @@ def main():
     gmap1.scatter(attractions_lats, attractions_lngs, color='#3B0B39', size=40, marker=False)
 
     gmap1.scatter(attractions_lats, attractions_lngs, 'cornflowerblue', edge_width = 3.0)
+
     gmap1.draw('map.html')
 
 if __name__ == "__main__":
